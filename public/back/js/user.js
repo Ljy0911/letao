8dae7c8a9cabdf734a93978b686996d7f7f3c5a8
ljy&pp <1902742810@qq.com>
2019-11-02 16:36:11 +0800

    用户管理-分页渲染完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,22 +1,44 @@
 
 $(function () {
+
+    var currentPage = 1;
+    var pageSize = 5;
     // 1. 一进入页面，发送 ajax 请求，
     // 获取用户列表数据，通过模板引擎渲染
-    $.ajax({
-        type: "get",
-        url: " /user/queryUser",
-        data: {
-            page: 1,
-            pageSize: 5
-        },
-        dataType: "json",
-        success: function (info) {
-            // console.log(info);
-            // template( 模板 id，数据对象 )
-            // 在模板中可以任意使用 数据对象中的属性
-            var htmlStr = template('tpl',info);
-            $('tbody').html( htmlStr );
-        },
-        
-    })
+    render();
+
+    function render() {
+        $.ajax({
+            type: "get",
+            url: " /user/queryUser",
+            data: {
+                page: currentPage,
+                pageSize: pageSize
+            },
+            dataType: "json",
+            success: function (info) {
+                // template( 模板 id，数据对象 )
+                // 在模板中可以任意使用 数据对象中的属性
+                var htmlStr = template('tpl', info);
+                $('tbody').html(htmlStr);
+
+                // 分页初始化
+                $('#paginator').bootstrapPaginator({
+                    // 配置 bootstrap 版本
+                    bootstrapMajorVersion: 3,//默认是2，如果是bootstrap3版本，这个参数必填
+                    currentPage: info.page,//当前页
+                    totalPages: Math.ceil(info.total / info.size),//总页数
+                    onPageClicked: function (event, originalEvent, type, page) {
+                        //为按钮绑定点击事件 page:当前点击的按钮值
+                        // console.log(page);
+                        currentPage = page;
+                        render();
+                    }
+                });
+            },
+
+        });
+    }
+
+
 })
