fe7c4d140ae43bc9eeec6e55e75c250a3cb669d8
ljy&pp <1902742810@qq.com>
2019-11-04 23:19:40 +0800

    二级分类-设置隐藏域进行提交

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -14,7 +14,7 @@ $(function () {
             },
             dataType: "json",
             success: function (info) {
-                console.log(info);
+                // console.log(info);
                 var htmlStr = template("secondTpl",info);
                 $("tbody").html(htmlStr);
                 
@@ -34,4 +34,57 @@ $(function () {
             }
         });
     }    
+
+    // 2. 点击添加分类按钮 显示模态框
+    $("#addBtn").click(function () {
+        $("#addModal").modal("show");
+
+        // 点击添加分类按钮时，发送ajax请求 获取 一级分类全部数据 通过模板引擎渲染
+        // 通过  page: 1, pageSize: 100 模拟获取全部数据分类数据的接口
+        $.ajax({
+            type: "get",
+            url: "/category/queryTopCategoryPaging",
+            data: {
+                page: 1,
+                pageSize: 100
+            },
+            dataType: "json",
+            success: function (info) {
+                // console.log(info);
+                var htmlStr = template("dropdownTpl",info);
+                $('.dropdown-menu').html(htmlStr);
+            }
+
+        });
+    });
+
+    // 3. 通过事件委托 给dropdown-menu 下面的所有a 绑定点击事件
+    $(".dropdown-menu").on("click","a",function () {
+        var txt = $(this).text();
+        $("#dropdownText").text(txt);
+
+        // 获取选中的id
+        var id = $(this).data("id");
+        // 设置谁input
+        $('[name="categoryId"]').val(id);
+    });
+
+    // 4. 进行文件上传初始化
+    $("#fileupload").fileupload({
+        // 配置返回的数据格式
+        dataType: "json",
+        // 图片上传完成后会调用 done 回调函数
+        // data: 图片上传后的对象，
+        // 通过data.result.picAddr 可以获取上传后的图片地址
+        done: function ( e, data ) {
+            // console.log(data.result.picAddr);
+            var imgUrl = data.result.picAddr;
+            $("#imgBox img").attr("src",imgUrl);
+
+            // 将图片地址设置给 input
+            $('[name="brandLogo"]').val(imgUrl);
+        }
+    });
+
+
 })
